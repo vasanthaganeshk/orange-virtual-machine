4170328acd841ed741abe24ae81e57cec8467950
Vasantha Ganesh K <vasanthaganesh.k@tutamail.com>
2016-11-12 14:32:02 +0530

    changed ovmDis's hard coding to commandline args

--- ovmDis.java
+++ ovmDis.java
@@ -1,11 +1,33 @@
+/*
+Java disassembler for Java.
+Copyright (C) 2016 Vasantha Ganesh K. <vasanthaganesh.k@tuta.io>.
+
+This file is part of orange-virtual-machine.
+
+orange-virtual-machine is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+orange-virtual-machine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with orange-virtual-machine.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import java.util.*;
 import java.io.*;
 import javassist.bytecode.*;
 import javassist.*;
 
 class ovmDis{
     public static void main(String[] args) throws IOException, BadBytecode{
+	String fname = args[0];
 	BufferedInputStream fin =
-	    new BufferedInputStream(new FileInputStream("hello_world.class"));
+	    new BufferedInputStream(new FileInputStream(fname));
 	ClassFile cf = new ClassFile(new DataInputStream(fin));
 	MethodInfo minfo = cf.getMethod("main");
 	CodeAttribute ca = minfo.getCodeAttribute();
