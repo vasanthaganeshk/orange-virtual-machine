d20e856e82e807a7aa28a2592406ceeb848b015b
Vasantha Ganesh K <vasanthaganesh.k@tutamail.com>
2016-11-13 21:32:38 +0530

    organized ovmDis, finding attributes for corresponding bytecode, separate runMethod, handling if main method is absent and more.

--- ovmDis.java
+++ ovmDis.java
@@ -18,30 +18,73 @@ You should have received a copy of the GNU General Public License
 along with orange-virtual-machine.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+import java.util.stream.*;
 import java.util.*;
 import java.io.*;
 import javassist.bytecode.*;
 import javassist.*;
 
+class IllegalClassFile extends Exception{
+  public IllegalClassFile(String message){
+     super(message);
+  }
+}
+
 class ovmDis{
-    public static void main(String[] args) throws Throwable{
-	String fname = args[0];
-	BufferedInputStream fin =
-	    new BufferedInputStream(new FileInputStream(fname));
-
-	ClassFile cf = new ClassFile(new DataInputStream(fin));	
-	MethodInfo minfo = cf.getMethod("main");
-	CodeAttribute ca = minfo.getCodeAttribute();
-
-	CodeIterator i = ca.iterator();
+    public static void runMethod(CodeIterator i, ConstPool cpl) throws Throwable {
 	while(i.hasNext()){
 	    int index = i.next();
 	    int op = i.byteAt(index);
 	    String cod = Mnemonic.OPCODE[op];
-	    System.out.println(cod);
 	    if(cod == "sipush"){
-		System.out.println(i.s16bitAt(index+1));
+		System.out.println(cod + " " + i.s16bitAt(index+1));
 	    }
+	    else if(cod == "istore_1"){
+		System.out.println(cod);
+	    }
+	    else if(cod == "istore_2"){
+		System.out.println(cod);
+	    }	    
+	    else if(cod == "getstatic"){
+		System.out.println(cod + " " + ((i.byteAt(index+1)<<8) + i.byteAt(index+2)));
+	    }
+	    else if(cod == "iload_1"){
+		System.out.println(cod);
+	    }
+	    else if(cod == "iload_2"){
+		System.out.println(cod);
+	    }
+	    else if(cod == "iadd"){
+		System.out.println(cod);
+	    }
+	    else if(cod == "invokevirtual"){
+		System.out.println(cod + " " + ((i.byteAt(index+1)<<8) + i.byteAt(index+2)));
+	    }
+	    else if(cod == "return"){
+		System.out.println(cod);
+	    }
+	    else{
+		System.out.println("Unhandled Bytecode " + cod);
+	    }
+	}
+    }
+    public static void main(String[] args) throws Throwable{
+	String fname = args[0];
+	BufferedInputStream fin = new BufferedInputStream(new FileInputStream(fname));
+	
+	ClassFile cf = new ClassFile(new DataInputStream(fin));	
+	ConstPool cpl = cf.getConstPool();
+	List<MethodInfo> allMethods = cf.getMethods();
+	List<String> mnames = allMethods.stream().map(x -> x.getName()).collect(Collectors.toList());
+
+	if(mnames.contains("main")){
+	    MethodInfo minfo = cf.getMethod("main");
+	    CodeAttribute ca = minfo.getCodeAttribute();
+	    CodeIterator i = ca.iterator();
+	    runMethod(i, cpl);
+	}
+	else{
+	    throw new IllegalClassFile("No main method found in " + fname);
 	}
 	fin.close();
     }
